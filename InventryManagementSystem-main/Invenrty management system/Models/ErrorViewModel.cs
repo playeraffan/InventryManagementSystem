2f79e16c11d59af1c7e0ebf9cb8f252c5b7b0bc7
M.Zia <ziaazeem2004@gmail.com>
2025-04-21 22:16:02 +0500

    Test commit with added txt file

---
+++ InventryManagementSystem-main/Invenrty management system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Invenrty_management_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
